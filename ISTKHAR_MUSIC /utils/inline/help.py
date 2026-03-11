9b9a61a20fe6ca8352885cf515739234c96d30d8
Rishu <vishwakarmarishabh366@gmail.com>
2026-03-11 13:37:39 +0530

    Update help.py

--- ISTKHAR_MUSIC /utils/inline/help.py
+++ ISTKHAR_MUSIC /utils/inline/help.py
@@ -1,16 +1,8 @@
-# -----------------------------------------------
-# 🔸 ISTKHAR_MUSIC Project
-# 🔹 Developed & Maintained by: ISTKHAR Bots (https://github.com/TEAM-ISTKHAR)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by TEAM-ISTKHAR
-# -----------------------------------------------
+#Rishu Rajput 
+
+#Jai bhawani Jai rajputana 
+
+
 
 
 from typing import Union
@@ -33,7 +25,7 @@ def help_pannel(_, START: Union[bool, int] = None):
 	    [
 	        InlineKeyboardButton(
                     text=_["H_B_34"],
-                    callback_data="help_callback hb10",
+                    callback_data="rajputRishu9bottonnn",
 		)
 	    ],    
             [
